6c33a5e3420f88a9b4747dc035e60673aaf9f5a5
ZoraliaGuillen <zguillena@gmail.com>
2023-07-21 13:05:55 -0300

    first commit

--- models/citiesmodel.js
+++ models/citiesmodel.js
@@ -4,7 +4,6 @@ const citiesSchema = new mongoose.Schema({
     name:{type:String, required:true},
     country:{type:String, required:true},
     continent:{type:String, required:true},
-    language:{type:String, required:true},
     description:{type:String, required:true},
     image:{type:String, required:true},
 })
